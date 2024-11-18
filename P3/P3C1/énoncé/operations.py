2f1402cc8bb7f0f9f8a627af51d727f7f5353ab1
giadei <giancarlo.de-ieso@teamictvs.ch>
2024-11-18 14:31:25 +0100

    P3C1
    Signed-off-by: giadei <giancarlo.de-ieso@teamictvs.ch>

--- P3/P3C1/énoncé/operations.py
+++ P3/P3C1/énoncé/operations.py
@@ -1,1 +1,9 @@
 # Écrivez votre code ici !
+
+def addition(a, b):
+    """Retourne la somme de deux nombres."""
+    return a + b
+
+def multiplication(a, b):
+    """Retourne le produit de deux nombres."""
+    return a * b
